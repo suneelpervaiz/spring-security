3fb94792b8c702cbc7522b10de61719970877a98
suneelpervaiz <suneelpervaiz@gmail.com>
2019-10-03 01:14:12 +0500

    testing my auto build

--- src/main/java/com/sp/security/security/SecurityApplication.java
+++ src/main/java/com/sp/security/security/SecurityApplication.java
@@ -2,9 +2,23 @@ package com.sp.security.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+
+@RestController
 public class SecurityApplication {
+
+
+    @RequestMapping("/yes")
+
+    public String home(){
+        return "hello suneel how are you";
+    }
+
+
 
 	public static void main(String[] args) {
 
